3f31b8f5f603d851fce84c1a9df80d13678d2221
yangwonpark <ailey@nate.com>
2021-05-11 15:24:17 +0900

    2. template 상속

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 // template 폴더 (view파일 path를 적어줌)
 nunjucks.configure('template', {
-    autoescape : true,          // 보안
+    autoescape : true,          // false로 사용 시, html 태그가 작동이 되어 보안상 위험하다 => html부분에서 | safe를 붙여주면 부분 허용 가능
     express : app               // 위에서 선언한 express()를 담은 변수
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ router.get('/products', (req, res) => {
 
     // render(뿌려줄 view 파일) => app.js에서 nunjucks로 선언한 path 이후의 경로만 적어주면 됨
     res.render('admin/products.html' , {
-        message : 'hello!!!!!!!!!!!!!',          // 여기서 선언한 message => html 파일의 {{ message }}
+        message : `<h1>태그가 출력됩니다.</h1>`,          // 여기서 선언한 message => html 파일의 {{ message }}     뺵틱으로 묶어주면 html 태그 사용가능
         online : 'express'
     });        
 });
